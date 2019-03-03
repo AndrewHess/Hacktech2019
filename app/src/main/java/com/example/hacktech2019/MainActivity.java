f0bb4d9d4354374032ee53b5ad10e6ca08048c2a
Andrew Hess <ahess3815@gmail.com>
2019-03-03 03:38:25 -0800

    "Fixed issue with drink counts going to zero after visiting settings."

--- app/src/main/java/com/example/hacktech2019/MainActivity.java
+++ app/src/main/java/com/example/hacktech2019/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.hacktech2019;
 
 import android.annotation.TargetApi;
 import android.app.Notification;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.icu.util.Calendar;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private final static int drinksPerText = 5;
 
     private NotificationUtils mNotificationUtils;
+    private SharedPreferences prefs;
 
     private int waterAmount = 0;
     private int alcAmount = 0;
@@ -51,14 +60,28 @@ public class MainActivity extends AppCompatActivity {
             waterAmount = savedInstanceState.getInt(WATER_AMOUNT_KEY);
         }
 
+        prefs = getSharedPreferences("DRINK_COUNTS", MODE_PRIVATE);
         mNotificationUtils = new NotificationUtils(this);
+
+        onResume();
+
         updateScreen();
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        System.out.println("Restoring old drink counts ... --------------------------------------");
+        alcAmount = prefs.getInt("alcoholCount", MODE_PRIVATE);
+        waterAmount = prefs.getInt("waterCount", MODE_PRIVATE);
+    }
+
     public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putInt(ALC_AMOUNT_KEY, alcAmount);
         savedInstanceState.putInt(WATER_AMOUNT_KEY, waterAmount);
-        super.onSaveInstanceState(savedInstanceState);
     }
 
     public void onSettingsClick(View view) {
@@ -82,6 +105,11 @@ public class MainActivity extends AppCompatActivity {
         if (alcAmount % drinksPerEmail == drinksPerEmail - 1) {
             emailBuddy();
         }
+
+        // Save the updated alcohol number to the shared preferences.
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("alcoholCount", alcAmount).commit();
+
         updateScreen();
     }
 
@@ -92,6 +120,11 @@ public class MainActivity extends AppCompatActivity {
         timeLastWater = getCurrentTime();
         // Get the counter.
         waterAmount++;
+
+        // Save the updated water number to the shared preferences.
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("waterCount", waterAmount).commit();
+
         updateScreen();
     }
 
