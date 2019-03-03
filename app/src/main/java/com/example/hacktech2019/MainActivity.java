926b6b7f3c73791af278755b0c30bee078b4e113
Akshay <ayeluri@caltech.edu>
2019-03-02 18:42:34 -0800

    Hope this works with email and notifs

--- app/src/main/java/com/example/hacktech2019/MainActivity.java
+++ app/src/main/java/com/example/hacktech2019/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.hacktech2019;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,12 +15,17 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-import static android.content.Context.MODE_PRIVATE;
-
 public class MainActivity extends AppCompatActivity {
 
+    private final static int drinksPerNote = 5;
+    private final static int drinksPerEmail = 5;
+    private NotificationUtils mNotificationUtils;
+    private int waterAmount = 0;
+    private int alcAmount = 0;
+    private String ALC_AMOUNT_KEY = "alc";
+    private String WATER_AMOUNT_KEY = "water";
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,45 +40,57 @@ public class MainActivity extends AppCompatActivity {
             //Toast.makeText(MainActivity.this, "First Run", Toast.LENGTH_LONG).show();
         }
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            alcAmount = savedInstanceState.getInt(ALC_AMOUNT_KEY);
+            waterAmount = savedInstanceState.getInt(WATER_AMOUNT_KEY);
+        }
+
+        mNotificationUtils = new NotificationUtils(this);
+        updateScreen();
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(ALC_AMOUNT_KEY, alcAmount);
+        savedInstanceState.putInt(WATER_AMOUNT_KEY, waterAmount);
+        super.onSaveInstanceState(savedInstanceState);
     }
 
     public void onSettingsClick(View view) {
         startActivity(new Intent(MainActivity.this, Setup.class));
-        setContentView(R.layout.activity_main);
     }
 
     /** Increment the alcohol counter. */
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public void onAlcoholClick(View view) {
         // Get the counter.
-        TextView textView = (TextView) findViewById(R.id.alcohol_counter);
-
-        Integer cur = Integer.parseInt(textView.getText().toString());
-        cur++;
-        textView.setText(Integer.toString(cur));
+        alcAmount++;
+        if (alcAmount % drinksPerNote == drinksPerNote - 1) {
+            makeNote();
+        }
+        if(alcAmount % drinksPerEmail == drinksPerEmail - 1){
+            emailBuddyNshots(alcAmount);
+        }
+        updateScreen();
     }
 
     /** Increment the water counter. */
     public void onWaterClick(View view) {
         // Get the counter.
-        TextView textView = (TextView) findViewById(R.id.water_counter);
-
-        Integer cur = Integer.parseInt(textView.getText().toString());
-        cur++;
-        textView.setText(Integer.toString(cur));
-
-        int drinksPerEmail = 2;
-        if(cur % drinksPerEmail == drinksPerEmail - 1){
-            emailBuddyNshots(cur);
-        }
+        waterAmount++;
+        updateScreen();
     }
 
+    public void updateScreen() {
+        ((TextView) findViewById(R.id.alcohol_counter)).setText(Integer.toString(alcAmount));
+        ((TextView) findViewById(R.id.water_counter)).setText(Integer.toString(waterAmount));
+    }
 
     public double bloodAlcohol(){
         return 0;
     }
 
     public void emailBuddyNshots(int nDrinks){
-
         SharedPreferences info = getSharedPreferences("USER_INFO", MODE_PRIVATE);
         String drunkName = info.getString("Name", "");
         String buddyEmail = info.getString("BuddyMail", "");
@@ -93,7 +115,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void emailBuddyBAC(){
-
         SharedPreferences info = getSharedPreferences("USER_INFO", MODE_PRIVATE);
         String drunkName = info.getString("Name", "");
         String buddyEmail = info.getString("BuddyMail", "");
@@ -115,5 +136,13 @@ public class MainActivity extends AppCompatActivity {
         } catch (android.content.ActivityNotFoundException ex) {
             Toast.makeText(MainActivity.this, "Yike :(", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void makeNote() {
+        String notice = "You had " + alcAmount + " standard drinks, and only " + waterAmount + " glasses of water!";
+        Notification.Builder nb = mNotificationUtils.
+                getAndroidChannelNotification("Alcohol notice", notice, R.drawable.drinky_icon);
+        mNotificationUtils.getManager().notify(101, nb.build());
     }
 }
